59bbf4af7c3f65d4cc06d0a86ee54f8618093ef3
Geoff Hayes <hayesgm@gmail.com>
2014-06-26 13:33:49 -0700

    Add migrate as an optional parameter

--- lib/opsworks/tasks/opsworks.rake
+++ lib/opsworks/tasks/opsworks.rake
@@ -2,14 +2,15 @@
 namespace :opsworks do
 
   desc 'Deploy to Opsworks'
-  task :deploy, [:rails_env] => :environment do |t, args|
+  task :deploy, [:rails_env,:migrate] => :environment do |t, args|
     rails_env = args[:rails_env] || ENV['RAILS_ENV']
-    
+    migrate = args[:migrate] == "true" || args[:migrate] == "t"
+
     raise ArgumentError, "Please pass rails_env as argument or set RAILS_ENV environment var" if rails_env.nil? || rails_env == ""
     
-    puts "Deploying #{rails_env}..."
+    puts "Deploying #{rails_env}#{migrate ? " and running migrations" : ""}..."
 
-    Opsworks::Deploy.deploy(rails_env: rails_env)
+    Opsworks::Deploy.deploy(rails_env: rails_env, migrate: migrate)
 
     puts "Finished successfully"
   end
--- spec/opsworks_deploy_spec.rb
+++ spec/opsworks_deploy_spec.rb
@@ -15,14 +15,14 @@ describe 'opsworks:deploy rake task' do
   end
 
   it "should call invoke with argument when given arg" do
-    expect(Opsworks::Deploy).to receive(:deploy).with(rails_env: 'test-1')
+    expect(Opsworks::Deploy).to receive(:deploy).with(rails_env: 'test-1', migrate: false)
     Rake::Task["opsworks:deploy"].invoke('test-1')
   end
 
   it "should call invoke with argument when given env" do
     begin
       ENV['RAILS_ENV'] = "test-2"
-      expect(Opsworks::Deploy).to receive(:deploy).with(rails_env: 'test-2')
+      expect(Opsworks::Deploy).to receive(:deploy).with(rails_env: 'test-2', migrate: false)
       Rake::Task["opsworks:deploy"].invoke
     ensure
       ENV['RAILS_ENV'] = nil
@@ -33,7 +33,7 @@ describe 'opsworks:deploy rake task' do
     expect { Rake::Task["opsworks:deploy"].invoke() }.to raise_error(ArgumentError)
   end
 
-  it "should run opsworks deploy" do
+  it "should run opsworks deploy with migrate" do
     begin
       config = {
         'test-3' => {
@@ -52,17 +52,57 @@ describe 'opsworks:deploy rake task' do
       expect(File).to receive(:exists?).and_return(true)
       expect(File).to receive(:read).and_return(config.to_json)
 
+      expected_params = { stack_id: "sid", app_id: "aid", command: {name: 'deploy', args: {"migrate" => [ "false" ] } } }
+
       # Mock the AWS features
       client = double("client")
-      expect(client).to receive(:create_deployment)
+      expect(client).to receive(:create_deployment).with(expected_params)
 
       ops_works = double("ops_works")
       expect(ops_works).to receive(:client).and_return(client)
+
 
       expect(AWS).to receive(:ops_works).and_return(ops_works)
 
       # Call rake task
       Rake::Task["opsworks:deploy"].invoke
+    ensure # clear ENV vars
+      ENV['RAILS_ENV'] = ENV['IAM_KEY'] = ENV['IAM_SECRET'] = nil
+    end
+  end
+
+  it "should run opsworks deploy without migrate" do
+    begin
+      config = {
+        'test-4' => {
+          stack_id: 'sid',
+          app_id: 'aid'
+        }
+      }
+
+      # Mock ENV vars
+      ENV['IAM_KEY'] = ENV['IAM_SECRET'] = "a"
+
+      # Allow rails-generated config
+      Rails = double("rails")
+      expect(Rails).to receive(:root).and_return(".").twice
+      expect(File).to receive(:exists?).and_return(true)
+      expect(File).to receive(:read).and_return(config.to_json)
+
+      expected_params = { stack_id: "sid", app_id: "aid", command: {name: 'deploy', args: {"migrate" => [ "true" ] } } }
+
+      # Mock the AWS features
+      client = double("client")
+      expect(client).to receive(:create_deployment).with(expected_params)
+
+      ops_works = double("ops_works")
+      expect(ops_works).to receive(:client).and_return(client)
+
+
+      expect(AWS).to receive(:ops_works).and_return(ops_works)
+
+      # Call rake task
+      Rake::Task["opsworks:deploy"].invoke("test-4","true")
     ensure # clear ENV vars
       ENV['RAILS_ENV'] = ENV['IAM_KEY'] = ENV['IAM_SECRET'] = nil
     end
